34b8f9d4ff0eb57774c8627e0810005ea654d8d1
Hemant Gharu <hemantgharu98@gmail.com>
2024-07-14 20:31:27 +0530

    some negligible changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import ImagesCards from './components/ImagesContainer/ImagesCards';
 import Navbar from './components/Header/Navbar';
 import Header from './components/Header/Header';
 import FilterButtons from './components/ImagesContainer/FilterButtons';
-import Example from './components/ImagesContainer/Example';
 
 function App() {
    return (
       <>
          <Navbar />
          <Header />
-         <Example/>
          <FilterButtons />
          <ImagesCards />
       </>
